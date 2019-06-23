7cf133238333acbedccf8c4874f469fc27e5ef5e
Rim Vilgalys <vilgalys@gmail.com>
2019-06-23 13:16:26 -0400

    icon created, new styles applied

--- saved-link.js
+++ saved-link.js
@@ -1,14 +1,16 @@
 class SavedLink extends HTMLElement {
   set data(data) {
-    this.innerHTML = `<div class="title">${data.title}</p>
-    <div class="domain">${data.domain}</p>`;
+    this.innerHTML = `<span class="saved-link-item saved-link-title">${
+      data.title
+    }</span>
+    <span class="saved-link-item saved-link-domain">${data.domain}</span>`;
     this.classList.add("saved-link");
     this.addEventListener("click", () => {
       window.open(data.url, "_blank");
     });
 
-    const subredditLink = document.createElement("div");
-    subredditLink.classList.add("subreddit-link");
+    const subredditLink = document.createElement("span");
+    subredditLink.classList.add("subreddit-link", "saved-link-item");
     subredditLink.innerText = data.subreddit_name_prefixed;
     subredditLink.addEventListener("click", () => {
       window.open(`http://reddit.com/${data.permalink}`, "_blank");
