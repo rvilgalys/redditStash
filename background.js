3675f80e49c97b30086b46365e929a05ced3eccc
Rim Vilgalys <vilgalys@gmail.com>
2019-06-29 12:02:13 -0400

    refactored to run fetch on all saved links

--- background.js
+++ background.js
@@ -1,39 +1,30 @@
 const redditFeedUrl = "https://www.reddit.com/prefs/feeds";
 
-const getRedditStashAPI = callback => {
-  chrome.storage.sync.get(["redditStashKey"], callback);
-};
-
-const setRedditStashAPI = apiKey => {
-  chrome.storage.sync.set({ redditStashKey: apiKey }, () => {
-    console.log(`setting apiKey to ${apiKey}`);
-  });
-};
-
-const extractKey = sampleURL => {
-  const runRegEx = /feed=(.*?)&user/;
-
-  const result = runRegEx.exec(sampleURL)[1];
-  console.log(result);
-  return result;
+const setRedditStashBaseURL = baseURL => {
+  chrome.storage.sync.set({ redditStashBaseURL: baseURL });
 };
 
 const getKeyFromFeed = feedURL => {
-  const xhr = new XMLHttpRequest();
+  chrome.storage.sync.get({ redditStashBaseURL: "" }, result => {
+    if (result.redditStashBaseURL !== "") return;
 
-  xhr.open("GET", feedURL, true);
-  xhr.responseType = "document";
-  xhr.onreadystatechange = request => {
-    if (xhr.readyState !== 4 || xhr.status !== 200) return;
-    const elements = request.currentTarget.response.getElementsByClassName(
-      "json-link"
-    );
-    const key = extractKey(elements[0].href);
+    const xhr = new XMLHttpRequest();
 
-    setRedditStashAPI(key);
-  };
+    xhr.open("GET", feedURL, true);
+    xhr.responseType = "document";
+    xhr.onreadystatechange = request => {
+      if (xhr.readyState !== 4 || xhr.status !== 200) return;
+      const elements = [
+        ...request.currentTarget.response.getElementsByClassName("json-link")
+      ];
+      const savedLinkFeed = elements.find(element => {
+        return /saved.json/.test(element);
+      });
+      setRedditStashBaseURL(savedLinkFeed.href);
+    };
 
-  xhr.send();
+    xhr.send();
+  });
 };
 
 getKeyFromFeed(redditFeedUrl);
--- fetchSaved.js
+++ fetchSaved.js
@@ -1,16 +1,67 @@
 import "./saved-link.js";
 
-window.addEventListener("load", () => {
-  fetchFeed();
+window.addEventListener("load", async () => {
+  //fetchFeed();
+  const stash = await fetchFromFeed({
+    limit: 100,
+    after: "",
+    runRecursive: true
+  });
+  console.log(stash);
 });
 
 const generateStashUrl = apiKey => {
   return `https://www.reddit.com/saved.json?feed=${apiKey}`;
 };
 
+const chromseStorageSyncGetAsync = keys => {
+  return new Promise((resolve, reject) => {
+    chrome.storage.sync.get([...keys], result => {
+      if (chrome.runtime.lastError) reject(chrome.runtime.lastError);
+      resolve(result);
+    });
+  });
+};
+
+const fetchFromFeed = async (
+  options = {
+    limit: 25,
+    after: "",
+    runRecursive: false
+  }
+) => {
+  try {
+    const baseURLObj = await chromseStorageSyncGetAsync(["redditStashBaseURL"]);
+    const baseURL = baseURLObj.redditStashBaseURL;
+    const url = new URL(baseURL);
+
+    url.searchParams.set("limit", options.limit);
+    if (options.after) url.searchParams.set("after", options.after);
+
+    const res = await fetch(url);
+    const json = await res.json();
+
+    if (json.data.after && options.runRecursive) {
+      const nextResult = await fetchFromFeed({
+        limit: options.limit,
+        after: json.data.after,
+        runRecursive: options.runRecursive
+      });
+      const combinedResult = [...json.data.children, ...nextResult];
+      return combinedResult;
+    }
+
+    return json.data.children;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
 async function fetchFeed() {
-  chrome.storage.sync.get(["redditStashKey"], async result => {
-    const res = await fetch(generateStashUrl(result.redditStashKey));
+  chrome.storage.sync.get(["redditStashBaseURL"], async result => {
+    console.log(result.redditStashBaseURL);
+
+    const res = await fetch(generateStashUrl(result.redditStashBaseURL));
     const json = await res.json();
 
     const main = document.querySelector("main");
